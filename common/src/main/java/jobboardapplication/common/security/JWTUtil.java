fb7d5aecff17d25956e6a89fa9d5e37c83e15bab
“mujahid341” <mujahidsaifuzzma@gmail.com>
2025-05-16 20:04:30 +0530

    JWTUtil class is created to generateToken , validateToken and for the extractEmail.

---
+++ common/src/main/java/jobboardapplication/common/security/JWTUtil.java
@@ -0,0 +1,49 @@
+package jobboardapplication.common.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private final Key key;
+    private final long expiration = 1000 * 60 * 60 * 24; // 24h
+    // 1000 milliseconds * 60 seconds * 60 minutes * 24 hours
+
+    public JWTUtil(@Value("${jwt.secret}") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isValid(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
